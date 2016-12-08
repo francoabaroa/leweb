f9df796731fe97fb1c9d6487a25b3dee6a9c7524
Franco Abaroa <franco9395@gmail.com>
2016-12-07 22:07:58 -0800

    Add npm modules and set up middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,56 @@
-'use strict';
-
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const metadata = require('../package.json');
+const compression = require('compression');
 const express = require('express');
+const path = require('path');
+const redis = require('redis');
+const sharp = require('sharp');
 
-// Constants
-const PORT = 8080;
+const utils = require('./lib/utilities.js');
+const bcrypt = require('bcrypt-nodejs');
+const session = require('express-session');
+const userController = require('../db/controllers/users.js');
+const bookmarkController = require('../db/controllers/bookmarks.js');
+const photoController = require('../db/controllers/photos.js');
+const likeController = require('../db/controllers/likes.js');
+const commentController = require('../db/controllers/comments.js');
 
-// App
+const port = process.env.NODE_PORT;
+const secret = process.env.SESSION_SECRET;
+const AWSaccessKey = process.env.ACCESSKEYID;
+const AWSsecretKey = process.env.SECRETACCESSKEY;
+
+// const redisClient = redis.createClient();
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+app.use(bodyParser.json({limit: '40mb'}));
+app.use(compression()); // gzip compress all responses
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: true
+}));
+
+const routes = ['/'];
+
+for (const route of routes) {
+  app.get(route, (req, res) => {
+    if (route === '/') {
+      res.redirect('/dashboard');
+    } else {
+      res.sendFile(path.join(__dirname, '/../client/index.html'));
+    }
+  });
+}
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+// wildcard route
+app.get('*', function(req, res) {
+  res.status(404).send('Not Found');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(port, () => {
+  console.log(`🌎  Listening on port ${port} for app ${metadata.name} 🌏`);
+});
