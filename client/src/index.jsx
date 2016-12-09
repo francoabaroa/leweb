9c38ddcc626e9f6ffe0cdba44d74e2fb212bf325
Franco Abaroa <franco9395@gmail.com>
2016-12-09 00:05:51 -0800

    Add new route for main component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,10 +6,13 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App.js';
 import Home from './components/Home.js';
+import Main from './components/Main.js';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App} >
+    </Route>
+    <Route path="/main" component={Main} >
     </Route>
   </Router>
 ), document.querySelector('.scene-container'));
