37adedc040bc28f07c533429894a2c3517f8dfb4
Franco Abaroa <franco9395@gmail.com>
2017-01-12 14:24:56 -0800

    Modify and add new component routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,12 +7,22 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App.js';
 import Home from './components/Home.js';
 import Main from './components/Main.js';
+import Portfolio from './components/Portfolio.js';
+import Skills from './components/Skills.js';
+import Contact from './components/Contact.js';
+import HackerWords from './components/HackerWords.js';
+import Immerse from './components/Immerse.js';
+import Goolp from './components/Goolp.js';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App} >
-    </Route>
-    <Route path="/main" component={Main} >
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/hackerwords" component={HackerWords} />
+      <Route path="/immerse" component={Immerse} />
+      <Route path="/goolp" component={Goolp} />
     </Route>
   </Router>
 ), document.querySelector('.scene-container'));
